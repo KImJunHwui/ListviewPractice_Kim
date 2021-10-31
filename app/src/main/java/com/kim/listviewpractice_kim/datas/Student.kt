d3049cc66269c4453eefe480e7057a5d1a0fa5d5
KJH <daeding7@naver.com>
2021-10-31 17:38:22 +0900

    학생 클래스 기능 추가 -> 숫자를 넣는 값에 따라, 로그를 다르게 찍는 기능

--- app/src/main/java/com/kim/listviewpractice_kim/datas/Student.kt
+++ app/src/main/java/com/kim/listviewpractice_kim/datas/Student.kt
@@ -1,5 +1,7 @@
 package com.kim.listviewpractice_kim.datas
 
+import android.util.Log
+
 
 // 학생 하위정보 - 이름, 출생년도
 
@@ -8,4 +10,35 @@ package com.kim.listviewpractice_kim.datas
 class Student(
     val name : String,
     val birthYear: Int) {
+
+
+//    학생의 기능? 예시
+//    자신의 이름을 로그로 찍는 기능.
+
+    fun printMyNameToLog(num1 : Int, num2 : Double) : Boolean {
+
+//        num1이 num2보다 크다면, 이름만 로그로
+//        그렇지 않다면, 이름과 인사도 로그 출력.
+
+        if (num1 > num2) {
+
+            Log.d("연습용", this.name)
+        return false
+
+
+        }
+        else {
+
+            Log.d("연습용")
+            Log.d("연습용", "잘 부탁드립니다")
+
+        }
+
+        return true
+
+
+
+    }
+
+
 }
